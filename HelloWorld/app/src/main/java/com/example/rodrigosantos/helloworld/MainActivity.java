4fce8543f35f13aa57712829fbaaaaec35c46b74
rodrigosantosti <rodrigosantos17silva@gmail.com>
2018-09-10 22:24:12 -0300

    nomeando o hello world

--- HelloWorld/app/src/main/java/com/example/rodrigosantos/helloworld/MainActivity.java
+++ HelloWorld/app/src/main/java/com/example/rodrigosantos/helloworld/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.rodrigosantos.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+/*
+    Rodrigo Silva dos Santos
+    Ra 816155559
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
